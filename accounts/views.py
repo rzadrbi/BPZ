493140067a640ae1981deb93fa8ad9420e043323
Reza <116594858+rzadrbi@users.noreply.github.com>
2024-10-28 23:56:24 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -24,3 +24,5 @@ class UserView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
